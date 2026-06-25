71e68f03b587419a4875ba8bf868f1671871c124
唐于斯 <1042220212@qq.com>
2026-06-25 18:03:39 +0800

    feat: add cesium 3d white model visualization

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,17 @@
-<script setup></script>
+<script setup>
+import CesiumScene from './components/CesiumScene.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <CesiumScene />
 </template>
 
-<style scoped></style>
+<style>
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
